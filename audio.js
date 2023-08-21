b7c09a40c284729a21541e9a05ad779b5b6d5c3b
Ron Ward <defroged@gmail.com>
2023-08-21 13:10:27 +0900

    separated files

---
+++ audio.js
@@ -0,0 +1,90 @@
+const audioContext = new(window.AudioContext || window.webkitAudioContext)();
+
+const createAudioBuffer = async (url) => {
+	const response = await fetch(url);
+	const arrayBuffer = await response.arrayBuffer();
+	const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+	return audioBuffer;
+};
+
+const playAudioBuffer = (buffer) => {
+	const source = audioContext.createBufferSource();
+	source.buffer = buffer;
+	source.connect(audioContext.destination);
+	source.start();
+};
+
+const playTenPointsSound = async () => {
+	const tenPointsAudioBuffer = await createAudioBuffer('audio/ten_points.mp3');
+	setTimeout(() => {
+    	playAudioBuffer(tenPointsAudioBuffer);
+	}, 10); 
+};
+
+const playUnderTenPointsSound = async () => {
+	const underTenPointsAudioBuffer = await createAudioBuffer('audio/under_ten_points.mp3');
+	setTimeout(() => {
+    	playAudioBuffer(underTenPointsAudioBuffer);
+	}, 10); 
+};
+
+let hitAudio, wrongAudio;
+
+const loadAudioFile = (src) => {
+	return new Promise((resolve) => {
+    	const audio = new Audio(src);
+    	audio.addEventListener('canplaythrough', () => {
+        	resolve(audio);
+    	});
+	});
+};
+
+const preloadWordAudioFiles = async () => {
+	const audioPromises = words.map((word) => {
+    	return loadAudioFile(`audio/${word}.mp3`).then((audio) => {
+        	wordAudioFiles[word] = audio;
+    	});
+	});
+
+	await Promise.all(audioPromises);
+};
+
+let remainingWords = [...words];
+
+const playWordAudio = () => {
+	// Refill the remainingWords array if it's empty
+	if (remainingWords.length === 0) {
+    	remainingWords = [...words];
+	}
+
+	// Pick a random word from the remainingWords array
+	const randomIndex = Math.floor(Math.random() * remainingWords.length);
+	currentWord = remainingWords[randomIndex];
+	remainingWords.splice(randomIndex, 1); // Remove the chosen word from the remainingWords array
+
+	const wordAudio = wordAudioFiles[currentWord];
+
+	if (!wordAudio) {
+    	console.error(`Audio not found for word: ${currentWord}`);
+    	return;
+	}
+
+	// Stop the current word audio before playing the new one
+	for (const audio of Object.values(wordAudioFiles)) {
+    	audio.pause();
+    	audio.currentTime = 0;
+	}
+
+	wordAudio.currentTime = 0;
+	wordAudio.play();
+};
+
+async function updateWordAudioFiles() {
+	const audioPromises = stage2Words.map((word) => {
+    	return loadAudioFile(`audio/${word}.mp3`).then((audio) => {
+        	wordAudioFiles[word] = audio;
+    	});
+	});
+
+	await Promise.all(audioPromises);
+}
---
+++ main.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+let animationStartTime = null;
+const init = async () => {
+    await preloadWordAudioFiles();
+
+    // Load the hit and wrong audio files
+    hitAudioBuffer = await createAudioBuffer('audio/hit.mp3');
+    wrongAudioBuffer = await createAudioBuffer('audio/wrong.mp3');
+
+    // Spawn word invaders
+    spawnWordInvaders();
+
+    // Add a click event listener to the canvas to start the game
+    canvas.addEventListener('click', startGame, {
+        once: true
+    });
+
+    // Show a "Click to start" message on the canvas
+    ctx.fillStyle = 'white';
+    ctx.font = '30px Orbitron';
+    ctx.fillText('Click to start', canvas.width / 2 - 80, canvas.height / 2);
+};
+
+const startGame = () => {
+    // Start the game loop
+    requestAnimationFrame(gameLoop);
+
+    // Play the first word audio
+    playWordAudio();
+};
+
+init();
